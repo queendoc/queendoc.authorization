d6e7329ed1a6a0c259a446bdc052864d73d56aba
umut.taherzadeh <masa1992>
2016-12-28 23:51:03 +0300

    login page

---
+++ src/app/components/login-umut/login.component.ts
@@ -0,0 +1,14 @@
+/**
+ * Created by umut.taherzadeh on 2016-12-28.
+ */
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+    title = 'app works!';
+}
