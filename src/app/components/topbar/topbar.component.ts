13b2ca97d1f032cc06d767128f38633e14a3e0c6
sh-queendoc <Tgy12Ujh>
2017-01-04 19:46:43 +0200

    Example Login Form

---
+++ src/app/components/topbar/topbar.component.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by shapour.taherzadeh on 2016-12-28.
+ */
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-topbar',
+    templateUrl: './topbar.component.html',
+    styleUrls: ['./topbar.component.css']
+})
+export class TopbarComponent {
+    title = 'Queendoc Top Bar';
+}
