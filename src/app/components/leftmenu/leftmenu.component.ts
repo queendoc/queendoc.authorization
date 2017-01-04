a05cdd701306f8545abc2956b474adf573d5fc2a
sh-queendoc <Tgy12Ujh>
2017-01-04 19:45:52 +0200

    Example Login Form

---
+++ src/app/components/leftmenu/leftmenu.component.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by shapour.taherzadeh on 2017-01-04.
+ */
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-leftmenu',
+    templateUrl: './leftmenu.component.html',
+    styleUrls: ['./leftmenu.component.css']
+})
+export class LeftMenuComponent {
+    title = 'Queendoc Left Menu';
+}
