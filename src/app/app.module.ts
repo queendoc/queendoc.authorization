0979a034260be4f9dad32ad273cc6693178b7fb0
sh-queendoc <Tgy12Ujh>
2017-01-04 19:47:14 +0200

    Example Login Form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,16 @@ import {HttpModule} from '@angular/http';
 
 import {AppComponent} from './app.component';
 import {LoginComponent} from "./components/login-umut/login.component";
+import {TopbarComponent} from "./components/topbar/topbar.component";
+import {LeftMenuComponent} from "./components/leftmenu/leftmenu.component";
 
 @NgModule({
     declarations: [
         AppComponent,
-        LoginComponent /** new components should be added **/
+        LoginComponent,
+        TopbarComponent,
+        LeftMenuComponent/** new components should be added **/
+
     ],
     imports: [
         BrowserModule,
@@ -17,7 +22,7 @@ import {LoginComponent} from "./components/login-umut/login.component";
         HttpModule
     ],
     providers: [],
-    bootstrap: [LoginComponent] /** to start login screen, it has been changed from AppComponent to LoginComponent **/
+    bootstrap: [TopbarComponent,LeftMenuComponent] /** to start login screen, it has been changed from AppComponent to LoginComponent **/
 })
 export class AppModule {
 }
